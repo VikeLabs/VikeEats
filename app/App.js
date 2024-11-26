3e926f8aa7fa4efb11ce7e93f2d5e8882d788978
ethan-jwlee <160280720+ethan-jwlee@users.noreply.github.com>
2024-11-26 13:14:41 -0800

    Add files via upload

--- app/App.js
+++ app/App.js
@@ -1,33 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import Button from './filter-button.js'; 
-import Dropdown from './filter-button-dropdown.js'; 
+import Navbar from './Navbar';
 
-const App = () => {
-  const handleClick = () => {
-    alert('Button clicked!');
-  };
-  
-  const options = [
-    { value: 'option1', label: 'Option 1' },
-    { value: 'option2', label: 'Option 2' },
-    { value: 'option3', label: 'Option 3' },
-  ];
-
-  const handleDropdownSelect = (selectedOption) => {
-    console.log('Selected Option:', selectedOption);
-  };
-
+function App() {
   return (
-    <div>
-      <Button 
-        label="Filter" 
-        onClick={handleClick} 
-      />
-	  <Dropdown options={options} onSelect={handleDropdownSelect} />
+    <div className="App">
+      <header className="App-header">
+        <Navbar />
+      </header>
     </div>
   );
-};
+}
 
 export default App;
