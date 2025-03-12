db36bc56cd4b722c337fd18724f769f2f01fcb1c
Charlotte Jacques <charlottej3333@gmail.com>
2025-03-12 16:11:39 -0700

    added search for dietary restrictions

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 # Import your blueprints
 from .food_outlets import food_outlets_blueprint
 from .menu import menu_blueprint
+from .search_menu import search_blueprint
 from .datetimeencoder import DateTimeEncoder
 import logging
 
@@ -14,6 +15,7 @@ def create_app():
     app.json_encoder = DateTimeEncoder
     app.register_blueprint(food_outlets_blueprint, url_prefix='/api')
     app.register_blueprint(menu_blueprint, url_prefix='/api')
+    app.register_blueprint(search_blueprint, url_prefix='/api')
     return app
 
 
